263e71d4a1d422414299aee837976c05b1679388
Orran <okrieg@bu.edu>
2023-12-24 22:31:13 +0000

    used standard assert

--- content/src/parser/test_simple_input.c
+++ content/src/parser/test_simple_input.c
@@ -2,14 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-#define TEST_ASSERT(x) do { \
-	if (!(x)) { \
-		fprintf(stderr, "%s:%d: Assertion (%s) failed!\n", \
-				__FILE__, __LINE__, #x); \
-	       	abort(); \
-	} \
-} while(0)
+#include <assert.h>
 
 int
 main(void)
@@ -17,20 +10,20 @@ main(void)
 	struct pipeline* my_pipeline = pipeline_build("ls\n");
 
 	// Test that a pipeline was returned
-	TEST_ASSERT(my_pipeline != NULL);
-	TEST_ASSERT(!my_pipeline->is_background);
-	TEST_ASSERT(my_pipeline->commands != NULL);
+	assert(my_pipeline != NULL);
+	assert(!my_pipeline->is_background);
+	assert(my_pipeline->commands != NULL);
 
 	// Test the parsed args
-	TEST_ASSERT(strcmp("ls", my_pipeline->commands->command_args[0]) == 0);
-	TEST_ASSERT(my_pipeline->commands->command_args[1] == NULL);
+	assert(strcmp("ls", my_pipeline->commands->command_args[0]) == 0);
+	assert(my_pipeline->commands->command_args[1] == NULL);
 
 	// Test the redirect state
-	TEST_ASSERT(my_pipeline->commands->redirect_in_path == NULL);
-	TEST_ASSERT(my_pipeline->commands->redirect_out_path == NULL);
+	assert(my_pipeline->commands->redirect_in_path == NULL);
+	assert(my_pipeline->commands->redirect_out_path == NULL);
 
 	// Test that there is only one parsed command in the pipeline
-	TEST_ASSERT(my_pipeline->commands->next == NULL);
+	assert(my_pipeline->commands->next == NULL);
 
 	pipeline_free(my_pipeline);
 }
