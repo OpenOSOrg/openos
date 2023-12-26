31f3816d2980b829f53412b18115031fab80a623
Orran <okrieg@bu.edu>
2023-12-26 02:04:23 +0000

    improved comments

--- content/src/parser/myshell_parser.h
+++ content/src/parser/myshell_parser.h
@@ -6,7 +6,7 @@
 #define MAX_ARGV_LENGTH (MAX_LINE_LENGTH / 2 + 1)
 
 struct pipeline_command {
-  char *command_args[MAX_ARGV_LENGTH]; // list of commands
+  char *command_args[MAX_ARGV_LENGTH]; // arg[0] is command, rest are arguments
   char *redirect_in_path;  // NULL or Name of file to redirect in from 
   char *redirect_out_path; // NULL or Name of a file to redirect out to
   struct pipeline_command *next; // next command in the pipeline. NULL if done 
